1e02350c1beda7b004a31314c5158ba7ae5f0881
faver3645 <143396926+faver3645@users.noreply.github.com>
2025-10-04 10:57:37 +0200

    Added migrations

--- obj/Debug/net8.0/ITPE3200FAM.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITPE3200FAM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITPE3200FAM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da5d91575fbdbccda39d268fe9704e48b082811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8e8aa2812d0bfb8235564291017476e461331e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITPE3200FAM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITPE3200FAM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
