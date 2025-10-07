d8796e9bdf0a14e80b03116af9a8d2ee721690a6
Muniira243 <mumoh7555@oslomet.no>
2025-10-07 20:45:24 +0200

    Repository navn endring

--- DAL/QQuizRepository.cs
+++ DAL/QQuizRepository.cs
@@ -1,13 +1,48 @@
+using Microsoft.EntityFrameworkCore;
 using MyShop.Models;
 
 namespace MyShop.DAL
 {
-    public interface QQuizRepository
+    public class QQuizRepository
     {
-        Task<IEnumerable<Quiz>> GetALL();
-        Task<Quiz?> GetItemById(int id);
-        Task Create(Quiz item);
-        Task Update(Quiz item);
-        Task<bool> Delete(int id);
+        private readonly QuizDbContext _db;
+
+        public QQuizRepository(QuizDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Quiz>> GetAllAsync()
+        {
+            return await _db.Quizzes.ToListAsync();
+        }
+
+        public async Task<Quiz?> GetQuizByIdAsync(int id)
+        {
+            return await _db.Quizzes.FindAsync(id);
+        }
+
+        public async Task CreateAsync(Quiz quiz)
+        {
+            _db.Quizzes.Add(quiz);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Quiz quiz)
+        {
+            _db.Quizzes.Update(quiz);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var quiz = await _db.Quizzes.FindAsync(id);
+            if (quiz == null)
+                return false;
+
+            _db.Quizzes.Remove(quiz);
+            await _db.SaveChangesAsync();
+            return true;
+        }
     }
 }
