3354ea2e472f9fa1942e0bd03a808f257b36f9a9
Muniira243 <mumoh7555@oslomet.no>
2025-10-07 20:30:30 +0200

    QuizRepository

--- DAL/QQuizRepository.cs
+++ DAL/QQuizRepository.cs
@@ -5,7 +5,7 @@ namespace MyShop.DAL
     public interface QQuizRepository
     {
         Task<IEnumerable<Quiz>> GetALL();
-        Task<Item?> GetItemById(int id);
+        Task<Quiz?> GetItemById(int id);
         Task Create(Quiz item);
         Task Update(Quiz item);
         Task<bool> Delete(int id);
